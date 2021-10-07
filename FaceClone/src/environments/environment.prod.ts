e110eb691127f6941c39f26594553b5591753954
Fernando Schimidt <fernandoschimidt@outlook.com>
2021-10-07 20:42:13 -0300

    face clone

---
+++ FaceClone/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDKB1LhIuuttTlfIK94BDoIieubqsLXIpE",
+    authDomain: "fb-clone-628f7.firebaseapp.com",
+    projectId: "fb-clone-628f7",
+    storageBucket: "fb-clone-628f7.appspot.com",
+    messagingSenderId: "966863069351",
+    appId: "1:966863069351:web:ef0fc03b3b6bea80423b82"
+  }
+};
---
+++ FaceClone/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDKB1LhIuuttTlfIK94BDoIieubqsLXIpE",
+    authDomain: "fb-clone-628f7.firebaseapp.com",
+    projectId: "fb-clone-628f7",
+    storageBucket: "fb-clone-628f7.appspot.com",
+    messagingSenderId: "966863069351",
+    appId: "1:966863069351:web:ef0fc03b3b6bea80423b82"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
